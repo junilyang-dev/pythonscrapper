488c1a87318735ea612968db30c674bf7f50867c
junilyang-dev <159020750+junilyang-dev@users.noreply.github.com>
2024-02-22 10:39:01 +0000

    python variable

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
-def say_hello(name):
-  print(f"hello {name} how r u?")
-say_hello("junil")
+def say_hello(name,age,you):
+  print(f"hello {name} and {age} years old how r u?",you)
+say_hello("junil",34,"arum")
+
+a = 2
+b = 3
+c = a + b
+a = 1
+b = 10
+print(c)
+
+my_name = "junil"
+print(my_name)
+
+a = "12"
+b = "34"
+c = a + b
+print(c)
+
+d = True
+print(d)
