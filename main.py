4edd4ef1e61d8212dc91446fa076ccff54eb69c1
junilyang-dev <159020750+junilyang-dev@users.noreply.github.com>
2024-02-22 11:13:09 +0000

    python Casino use boolean, print, input, if, while

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from random import randint
-user_choise = int(input("Enter a number: "))
-pc_choise = randint(1,50)
+print("Welcome to the Python Casino!")
 
-if user_choise == pc_choise:
-  print("You won!")
-elif user_choise > pc_choise:
-  print("Lower! Computer Chosen", pc_choise)
-elif user_choise < pc_choise:
-  print("Higher! Computer Chosen", pc_choise)
+pc_choise = randint(1,50) # I imported this
+
+playing = True
+
+while playing:
+  user_choise = int(input("Enter a number: "))
+  if user_choise == pc_choise:
+    print("You won!")
+    playing = False
+  elif user_choise > pc_choise:
+    print("Lower!")
+  elif user_choise < pc_choise:
+    print("Higher!")
+  
