8092b5218308bc73eeb5829ca503fcc69d2d678a
junilyang-dev <159020750+junilyang-dev@users.noreply.github.com>
2024-02-23 08:32:24 +0000

    Jobs

--- main.py
+++ main.py
@@ -8,6 +8,12 @@ response = requests.get(url)
 #print(response.content)
 
 soup = BeautifulSoup(response.content, "html.parser",)
-jobs = soup.find("section", class_="jobs").find_all("li")
+jobs = soup.find("section", class_="jobs").find_all("li")[1:-1]
 
-print(jobs)
+for job in jobs:
+  title = job.find("span", class_="title").text
+  company, position, region = job.find_all("span", class_="company")
+  conpany = company.text
+  position = position.text
+  region = region.text
+  print(title,  conpany, position, region, "----\n")
