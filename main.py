5927086f72e53a81686d909d5d6d22b1df9d245e
junilyang-dev <159020750+junilyang-dev@users.noreply.github.com>
2024-02-23 07:29:10 +0000

    class method

--- main.py
+++ main.py
@@ -1,17 +1,29 @@
-from random import randint
-print("Welcome to the Python Casino!")
+class Puppy:
 
-pc_choise = randint(1,50) # I imported this
+  def __init__(self, name, color):
+    self.name = name
+    self.age = 0.1
+    self.color = color
 
-playing = True
+  def __str__(self):
+    return f"Puppy name: {self.name}, age: {self.age}, color: {self.color}"
 
-while playing:
-  user_choise = int(input("Enter a number: "))
-  if user_choise == pc_choise:
-    print("You won!")
-    playing = False
-  elif user_choise > pc_choise:
-    print("Lower!")
-  elif user_choise < pc_choise:
-    print("Higher!")
-  
+  def woof_woof(self):
+    print(f"{self.name} woof woof!")
+
+  def introduce(self):
+    self.woof_woof()
+    print(f"Hi, my name is {self.name} and I'm a {self.color} puppy.")
+
+ruffus = Puppy(
+  name="Ruffus", 
+  color="brown",
+)
+bibi = Puppy(
+  name="Bibi", 
+  color="white",
+)
+
+print(ruffus, bibi)
+ruffus.introduce()
+bibi.introduce()
