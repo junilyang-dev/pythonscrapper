e1d2282c8d9684c982521f0db0fe90d40ef35b32
junilyang-dev <159020750+junilyang-dev@users.noreply.github.com>
2024-02-24 11:12:01 +0000

    job_data append list and add url

--- main.py
+++ main.py
@@ -7,13 +7,21 @@ response = requests.get(url)
 
 #print(response.content)
 
+all_jobs = []
+
 soup = BeautifulSoup(response.content, "html.parser",)
 jobs = soup.find("section", class_="jobs").find_all("li")[1:-1]
 
 for job in jobs:
   title = job.find("span", class_="title").text
   company, position, region = job.find_all("span", class_="company")
-  conpany = company.text
-  position = position.text
-  region = region.text
-  print(title,  conpany, position, region, "----\n")
+  url = job.find("div", class_="tooltip--flag-logo").next_sibling["href"]
+  job_data = {
+    "title": title,
+    "company": company.text,
+    "position": position.text,
+    "region": region.text,
+    "url" : f"https://weworkremotely.com{url}",
+  }
+  all_jobs.append(job_data)
+  print(all_jobs)
