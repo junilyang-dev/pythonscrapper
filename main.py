cbaa9224a1f946f489874eefffd1ddcb00cbebe6
junilyang-dev <159020750+junilyang-dev@users.noreply.github.com>
2024-02-26 09:50:24 +0000

    remoteok.com scrapping

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import requests 
 from bs4 import BeautifulSoup
-
-all_jobs = []#리스트 생성
+'''
+all_jobs = []
 
 #페이징 페이지가 있으므로 스크랩 기능을 함수로 만들어서 사용
 def scrape_page(url):
   response = requests.get(url)#페이지 요청
   soup = BeautifulSoup(response.content, "html.parser",)#페이지 내용을 파싱
-  jobs = soup.find("section", class_="jobs").find_all("li")[1:-1]]#section class jobs의 li태그를 찾아서 li태그의 1번째부터 마지막까지 찾아서 jobs라는 변수에 저장
+  jobs = soup.find("section", class_="jobs").find_all("li")[1:-1]#페이지 내용에서 필요한 정보를 추출
 
   for job in jobs:
     title = job.find("span", class_="title").text#제목
@@ -37,3 +37,38 @@ for x in range(total_pages):
 
 
 print(len(all_jobs))#모든 정보를 가져왔으므로 개수를 출력
+'''
+
+keyword = [
+  "flutter",
+  "phthon",
+  "golang"
+]
+
+all_jobs = []
+
+def scrape_page(url,x):
+  r = requests.get(
+    "https://remoteok.com/remote-golang-jobs",headers={
+      "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36"
+    })
+  
+  soup = BeautifulSoup(r.text, "html.parser")
+  jobs = soup.find("table", id="jobsboard").find_all("tr", class_="job")
+  
+  for job in jobs:
+    print("----------------------------------")
+    title = job.find("td", class_="company").find("h2", itemprop="title").text.replace('\n', '')
+    company = job.find("td", class_="company").find("h3", itemprop="name").text.replace('\n', '')
+    job_data = {
+      "language":x,
+      "title": title,
+      "company": company
+    }
+    all_jobs.append(job_data)#모든 정보를 리스트에 저장
+
+for x in keyword:
+  url = f"https://remoteok.com/remote-{x}-jobs"#페이지를 가져오는 url
+  scrape_page(url,x)#페이지를 스크랩하는 함수를 호출
+
+print(all_jobs)
