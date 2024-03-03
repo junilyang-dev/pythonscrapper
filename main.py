8405e545940e5131cb60016d2526f0923051dba7
junilyang-dev <159020750+junilyang-dev@users.noreply.github.com>
2024-03-03 12:11:42 +0000

    start Flask

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
+from flask import Flask
 from extractors.indeed import extract_indeed_jobs
 from extractors.wwr import extract_wwr_jobs
 from file import save_to_file
 
-keyword = input("What do you want to search for?")
+app = Flask("JobScrapper")
 
-#indeed = extract_indeed_jobs(keyword)
-wwr = extract_wwr_jobs(keyword)
-#jobs = indeed + wwr
-jobs = wwr
-save_to_file(keyword, jobs)
+@app.route("/")
+def home():
+  return 'hey there!'
+
+app.run("0.0.0.0")
+
+# keyword = input("What do you want to search for?")
+
+# #indeed = extract_indeed_jobs(keyword)
+# wwr = extract_wwr_jobs(keyword)
+# #jobs = indeed + wwr
+# jobs = wwr
+# save_to_file(keyword, jobs)
