dc5cddf27d3c02ea7b8df4062702c744626c658e
junilyang-dev <159020750+junilyang-dev@users.noreply.github.com>
2024-03-03 12:13:42 +0000

    start Flask remark

--- main.py
+++ main.py
@@ -2,13 +2,15 @@ from flask import Flask
 from extractors.indeed import extract_indeed_jobs
 from extractors.wwr import extract_wwr_jobs
 from file import save_to_file
-
+#Flask 객체 생성
 app = Flask("JobScrapper")
-
+#루트 라우팅
 @app.route("/")
+#루트 함수
 def home():
+  #루트 함수 반환
   return 'hey there!'
-
+#루트 함수 실행
 app.run("0.0.0.0")
 
 # keyword = input("What do you want to search for?")
